7b4ce8d0a98cb95fd1c2fee6cf2091fcd2b9e0fa
Martin Stanek <stanekmartin@bluewin.ch>
2020-06-08 12:01:37 +0200

    Small steps

--- Droid/WebViewWASM/app/src/main/java/com/example/webviewwasm/MainActivity.java
+++ Droid/WebViewWASM/app/src/main/java/com/example/webviewwasm/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.webviewwasm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.TargetApi;
 import android.os.Bundle;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient() {
+
+            @SuppressWarnings("deprecation")
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+            }
+
+            @TargetApi(android.os.Build.VERSION_CODES.M)
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest req, WebResourceError rerr) {
+                onReceivedError(view, rerr.getErrorCode(), rerr.getDescription().toString(), req.getUrl().toString());
+            }
+        });
+
+        webView.loadUrl("https://www.google.com");
     }
 }
