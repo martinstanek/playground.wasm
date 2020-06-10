e6c5b053f387528fca257e4b1bfd81241df30d76
Martin Stanek <stanekmartin@bluewin.ch>
2020-06-10 12:32:14 +0200

    WASM actually works on Droid!

---
+++ Droid/WebViewWASM/app/src/main/assets/WASM/interop.js
@@ -0,0 +1,14 @@
+function callWasm(name)
+{
+    document.getElementById("output").value = "calling wasm ...";
+    document.getElementById("output").value = sayHelloWasm(name);
+    
+    webkit.messageHandlers.callBack.postMessage(document.getElementById("output").value);
+}
+
+function sayHelloWasm(name)
+{
+  const sayHello = Module.mono_bind_static_method("[example] Example:SayHello");
+
+  return sayHello(name);
+}
